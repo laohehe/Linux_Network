1b06cac9f3ff87c199ece3712275234abbc8baba
He Yun Fei <1064521309@qq.com>
2019-10-29 22:57:58 +0800

    Update select_server.c
    学习阶段性提交代码

--- select/select_server.c
+++ select/select_server.c
@@ -2,6 +2,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/types.h>
+#include <arpa/inet.h>
 //#include <stdbool.h>
 
 #define bool int
@@ -87,19 +88,64 @@ int main()
 			continue;      //继续循环
 		}
 		//检查集合内的每个socket连接
-		for (int i = 0; i < conn_amount; i++)
+		for (i = 0; i < conn_amount; i++)
 		{
 			if (FD_ISSET(fd_A[i], &fdsr))
 			{
 				ret = recv(fd_A[i], buf, sizeof(buf), 0);
-				if (ret <= 0)
+				if (ret <= 0)             //客户端关闭
 				{
 					printf("client[%d] close\n", i);
 					close(fd_A[i]);
 					FD_CLR(fd_A[i], &fdsr);    //将该文件描述符从文件描述符集中清除
 					fd_A[i] = 0;
 				}
+				else     //输出接收到的数据
+				{
+					if (ret < BUF_SIZE)
+					{
+						memset(&buf[ret], '\0', 1);
+					}
+					printf("client[%d] send:%s\n", i, buf);
+				}
 			}
 		}
+
+		//检查是否有新连接接入
+		if (FD_ISSET(sock_fd, &fdsr))
+		{
+			new_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &sin_size);
+			if (new_fd <= 0)
+			{
+				perror("accept error!");
+				continue;
+			}
+			//增加至队列
+			if (conn_amount < BACKLOG)
+			{
+				fd_A[conn_amount++] = new_fd;
+				printf("new connection client[%d] %s:%d\n", conn_amount, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+				if (new_fd > maxsock)
+				{
+					maxsock = new_fd;
+				}
+			}
+			else
+			{
+				printf("已达到最大连接数，退出\n");
+				send(new_fd, "bye", 4, 0);
+				close(new_fd);
+				break;
+			}
+		}
+		showclient();
 	}
+
+	//关闭其他连接
+	for (i = 0; i < BACKLOG; i++)
+	{
+		if (fd_A[i] != 0)
+			close(fd_A[i]);
+	}
+	exit(0);
 }
